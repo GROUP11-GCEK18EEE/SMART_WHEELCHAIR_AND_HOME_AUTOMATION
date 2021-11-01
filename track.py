3a96ce61f80d89e8f4fdfdf5014b63076229803d
Shravan Sreedeep <shravansreedeep2000@gmail.com>
2021-11-01 22:44:04 +0530

    Some Programs For Reference

---
+++ track.py
@@ -0,0 +1,5 @@
+import cv2
+import numpy
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+img = cv2.imread(“your_image_name.jpeg”)
