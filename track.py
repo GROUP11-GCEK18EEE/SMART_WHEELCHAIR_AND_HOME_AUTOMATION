a3ac02fb44709b0d0d0eed211c65a42cc2931952
Shravan Sreedeep <shravansreedeep2000@gmail.com>
2021-11-07 23:04:49 +0530

    eye tracker program

--- track.py
+++
@@ -1,5 +0,0 @@
-import cv2
-import numpy
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-img = cv2.imread(“your_image_name.jpeg”)
