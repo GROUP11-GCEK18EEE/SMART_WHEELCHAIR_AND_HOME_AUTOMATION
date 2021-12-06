a2ef8dcfb0399649782fb893a7f4a912e092f88e
Shravan Sreedeep <shravansreedeep2000@gmail.com>
2021-12-06 23:46:38 +0530

    Program for eye detection developed and folder is arranged
    Up and Down movement of pupil is recognized along with right, left, center and blink.

---
+++ Eye Tracker Program/Eyetrackerandcontrollermain.py
@@ -0,0 +1,315 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+import math
+import numpy as np
+
+# variables 
+frame_counter =0
+CLOSED_TIME=0
+CEF_COUNTER =0
+TOTAL_BLINKS =0
+
+# colors 
+# values =(blue, green, red) opencv accepts BGR values not RGB
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (255,0,0)
+RED = (0,0,255)
+CYAN = (255,255,0)
+YELLOW =(0,255,255)
+MAGENTA = (255,0,255)
+GRAY = (128,128,128)
+GREEN = (0,255,0)
+PURPLE = (128,0,128)
+ORANGE = (0,165,255)
+PINK = (147,20,255)
+points_list =[(200, 300), (150, 150), (400, 200)]
+def drawColor(img, colors):
+    x, y = 0,10
+    w, h = 20, 30
+    
+    for color in colors:
+        x += w+5 
+
+def colorBackgroundText(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0), bgColor=(0,0,0), pad_x=3, pad_y=3):
+    (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
+    x, y = textPos
+    cv.rectangle(img, (x-pad_x, y+ pad_y), (x+t_w+pad_x, y-t_h-pad_y), bgColor,-1) # draw rectangle 
+    cv.putText(img,text, textPos,font, fontScale, textColor,textThickness ) # draw in text
+
+    return img
+
+def textWithBackground(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0), bgColor=(0,0,0), pad_x=3, pad_y=3, bgOpacity=0.5):
+    (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
+    x, y = textPos
+    overlay = img.copy() # coping the image
+    cv.rectangle(overlay, (x-pad_x, y+ pad_y), (x+t_w+pad_x, y-t_h-pad_y), bgColor,-1) # draw rectangle 
+    new_img = cv.addWeighted(overlay, bgOpacity, img, 1 - bgOpacity, 0) # overlaying the rectangle on the image.
+    cv.putText(new_img,text, textPos,font, fontScale, textColor,textThickness ) # draw in text
+    img = new_img
+
+    return img
+
+def textBlurBackground(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0),kneral=(33,33) , pad_x=3, pad_y=3):
+    (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
+    x, y = textPos
+    blur_roi = img[y-pad_y-t_h: y+pad_y, x-pad_x:x+t_w+pad_x] # croping Text Background
+    img[y-pad_y-t_h: y+pad_y, x-pad_x:x+t_w+pad_x]=cv.blur(blur_roi, kneral)  # merging the blured background to img
+    cv.putText(img,text, textPos,font, fontScale, textColor,textThickness )          
+    # cv.imshow('blur roi', blur_roi)
+    # cv.imshow('blured', img)
+
+    return img
+
+def fillPolyTrans(img, points, color, opacity):
+    list_to_np_array = np.array(points, dtype=np.int32)
+    overlay = img.copy()  # coping the image
+    cv.fillPoly(overlay,[list_to_np_array], color )
+    new_img = cv.addWeighted(overlay, opacity, img, 1 - opacity, 0)
+    # print(points_list)
+    img = new_img
+    cv.polylines(img, [list_to_np_array], True, color,1, cv.LINE_AA)
+    return img
+
+def rectTrans(img, pt1, pt2, color, thickness, opacity):
+    overlay = img.copy()
+    cv.rectangle(overlay, pt1, pt2, color, thickness)
+    new_img = cv.addWeighted(overlay, opacity, img, 1 - opacity, 0) # overlaying the rectangle on the image.
+    img = new_img
+
+    return img
+
+# constants
+CLOSED_EYES_FRAME =3
+FONTS =cv.FONT_HERSHEY_COMPLEX
+
+# Left eyes indices 
+LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
+LEFT_EYEBROW =[ 336, 296, 334, 293, 300, 276, 283, 282, 295, 285 ]
+
+# right eyes indices
+RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
+RIGHT_EYEBROW=[ 70, 63, 105, 66, 107, 55, 65, 52, 53, 46 ]
+
+map_face_mesh = mp.solutions.face_mesh
+
+# camera object 
+camera = cv.VideoCapture(1)
+
+# landmark detection function 
+def landmarksDetection(img, results, draw=False):
+    img_height, img_width= img.shape[:2]
+    mesh_coord = [(int(point.x * img_width), int(point.y * img_height)) for point in results.multi_face_landmarks[0].landmark]
+    if draw :
+        [cv.circle(img, p, 2, (0,255,0), -1) for p in mesh_coord]
+
+    # returning the list of tuples for each landmarks 
+    return mesh_coord
+
+# Euclaidean distance 
+def euclaideanDistance(point, point1):
+    x, y = point
+    x1, y1 = point1
+    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    return distance
+
+# Blinking Ratio
+def blinkRatio(img, landmarks, right_indices, left_indices):
+    # Right eyes 
+    # horizontal line 
+    rh_right = landmarks[right_indices[0]]
+    rh_left = landmarks[right_indices[8]]
+    # vertical line 
+    rv_top = landmarks[right_indices[12]]
+    rv_bottom = landmarks[right_indices[4]]
+    # draw lines on right eyes 
+    cv.line(img, rh_right, rh_left, GREEN, 2)
+    cv.line(img, rv_top, rv_bottom, WHITE, 2)
+
+
+    # LEFT_EYE 
+    # horizontal line 
+    lh_right = landmarks[left_indices[0]]
+    lh_left = landmarks[left_indices[8]]
+
+    # vertical line 
+    lv_top = landmarks[left_indices[12]]
+    lv_bottom = landmarks[left_indices[4]]
+
+    # draw lines on left eyes 
+    cv.line(img, lh_right, lh_left, GREEN, 2)
+    cv.line(img, lv_top, lv_bottom, WHITE, 2)
+
+    rhDistance = euclaideanDistance(rh_right, rh_left)
+    rvDistance = euclaideanDistance(rv_top, rv_bottom)
+
+    lvDistance = euclaideanDistance(lv_top, lv_bottom)
+    lhDistance = euclaideanDistance(lh_right, lh_left)
+
+    reRatio = rhDistance/rvDistance
+    leRatio = lhDistance/lvDistance
+
+    ratio = (reRatio+leRatio)/2
+    return ratio 
+
+    # Eyes Extrctor function,
+def eyesExtractor(img, right_eye_coords, left_eye_coords):
+    # converting color image to  scale image 
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # getting the dimension of image 
+    dim = gray.shape
+
+    # creating mask from gray scale dim
+    mask = np.zeros(dim, dtype=np.uint8)
+
+    # drawing Eyes Shape on mask with white color 
+    cv.fillPoly(mask, [np.array(right_eye_coords, dtype=np.int32)], 255)
+    cv.fillPoly(mask, [np.array(left_eye_coords, dtype=np.int32)], 255)
+
+    # draw eyes image on mask, where white shape is 
+    eyes = cv.bitwise_and(gray, gray, mask=mask)
+    # change black color to gray other than eys 
+    eyes[mask==0]=155
+    
+    # getting minium and maximum x and y  for right and left eyes 
+    # For Right Eye 
+    r_max_x = (max(right_eye_coords, key=lambda item: item[0]))[0]
+    r_min_x = (min(right_eye_coords, key=lambda item: item[0]))[0]
+    r_max_y = (max(right_eye_coords, key=lambda item : item[1]))[1]
+    r_min_y = (min(right_eye_coords, key=lambda item: item[1]))[1]
+
+    # For LEFT Eye
+    l_max_x = (max(left_eye_coords, key=lambda item: item[0]))[0]
+    l_min_x = (min(left_eye_coords, key=lambda item: item[0]))[0]
+    l_max_y = (max(left_eye_coords, key=lambda item : item[1]))[1]
+    l_min_y = (min(left_eye_coords, key=lambda item: item[1]))[1]
+
+    # croping the eyes from mask 
+    cropped_right = eyes[r_min_y: r_max_y, r_min_x: r_max_x]
+    cropped_left = eyes[l_min_y: l_max_y, l_min_x: l_max_x]
+
+    # returning the cropped eyes 
+    return cropped_right, cropped_left
+
+# creating pixel counter function 
+def pixelCounter(first_piece, second_piece, third_piece,fourth_piece,fifth_piece):
+    # counting black pixel in each part 
+    right_part = np.sum(first_piece==0)
+    center_part = np.sum(second_piece==0)
+    left_part = np.sum(third_piece==0)
+    up_part = np.sum(fourth_piece==0)
+    down_part = np.sum(fifth_piece==0)
+    # creating list of these values
+    eye_parts = [right_part, center_part, left_part, up_part, down_part]
+
+    # getting the index of max values in the list 
+    max_index = eye_parts.index(max(eye_parts))
+    pos_eye ='' 
+    if max_index==0:
+        pos_eye="RIGHT"
+        color=[BLACK, GREEN]
+    elif max_index==1:
+        pos_eye = 'CENTER'
+        color = [YELLOW, PINK]
+    elif max_index ==2:
+        pos_eye = 'LEFT'
+        color = [GRAY, YELLOW]
+    elif max_index ==3:
+        pos_eye = 'UP'
+        color = [GRAY, YELLOW]
+    elif max_index ==4:
+        pos_eye = 'DOWN'
+        color = [BLACK, YELLOW]
+    else:
+        pos_eye="CLOSED"
+        color = [WHITE, YELLOW]
+    return pos_eye, color
+
+
+# Eyes Postion Estimator 
+def positionEstimator(cropped_eye):
+    # getting height and width of eye 
+    h, w =cropped_eye.shape
+    
+    # remove the noise from images
+    gaussain_blur = cv.GaussianBlur(cropped_eye, (9,9),0)
+    median_blur = cv.medianBlur(gaussain_blur, 3)
+
+    # applying thrsholding to convert binary_image
+    ret, threshed_eye = cv.threshold(median_blur, 130, 255, cv.THRESH_BINARY)
+
+    # create fixd part for eye with 
+    piece = int(w/3) 
+    place = int(h/2)
+    # slicing the eyes into three parts 
+    right_piece = threshed_eye[0:h, 0:piece]
+    center_piece = threshed_eye[0:h, piece: piece+piece]
+    left_piece = threshed_eye[0:h, piece +piece:w]
+    up_piece=threshed_eye[0:place, 0:w]
+    down_piece=threshed_eye[place:place+place, 0:w]
+    
+    # calling pixel counter function
+    eye_position, color = pixelCounter(right_piece, center_piece, left_piece, up_piece, down_piece)
+
+    return eye_position, color 
+
+
+with map_face_mesh.FaceMesh(min_detection_confidence =0.5, min_tracking_confidence=0.5) as face_mesh:
+
+    # starting time here 
+    start_time = time.time()
+    # starting Video loop here.
+    while True:
+        frame_counter +=1 # frame counter
+        ret, frame = camera.read() # getting frame from camera 
+        #  resizing frame
+        
+        frame = cv.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv.INTER_CUBIC)
+        frame_height, frame_width= frame.shape[:2]
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+        results  = face_mesh.process(rgb_frame)
+        if results.multi_face_landmarks:
+            mesh_coords = landmarksDetection(frame, results, False)
+            ratio = blinkRatio(frame, mesh_coords, RIGHT_EYE, LEFT_EYE)
+            colorBackgroundText(frame,  f'Ratio : {round(ratio,2)}', FONTS, 0.7, (30,100),2, PINK, YELLOW)
+
+            if ratio >4.5:
+                CEF_COUNTER +=1
+                CLOSED_TIME +=1
+                colorBackgroundText(frame,  f'Blink', FONTS, 1.7, (int(frame_height/2), 100), 2, YELLOW, pad_x=6, pad_y=6, )
+
+            else:
+                if CEF_COUNTER>CLOSED_EYES_FRAME:
+                    TOTAL_BLINKS +=1
+                    CEF_COUNTER =0
+            colorBackgroundText(frame,  f'Total Blinks: {TOTAL_BLINKS} Total CEF={CEF_COUNTER} Total CLOSED TIME={CLOSED_TIME}', FONTS, 0.7, (30,150),2)
+
+            if TOTAL_BLINKS>2:
+                TOTAL_BLINKS=0
+                CLOSED_TIME=0
+            
+            cv.polylines(frame,  [np.array([mesh_coords[p] for p in LEFT_EYE ], dtype=np.int32)], True, GREEN, 1, cv.LINE_AA)
+
+            # Blink Detector Counter Completed
+            right_coords = [mesh_coords[p] for p in RIGHT_EYE]
+            left_coords = [mesh_coords[p] for p in LEFT_EYE]
+            crop_right, crop_left = eyesExtractor(frame, right_coords, left_coords)
+            eye_position_right, color = positionEstimator(crop_right)
+            colorBackgroundText(frame, f'R: {eye_position_right}', FONTS, 1.0, (40, 220), 2, color[0], color[1], 8, 8)
+            eye_position_left, color = positionEstimator(crop_left)
+            colorBackgroundText(frame, f'L: {eye_position_left}', FONTS, 1.0, (40, 320), 2, color[0], color[1], 8, 8)
+             
+             
+        # calculating  frame per seconds FPS
+        end_time = time.time()-start_time
+        fps = frame_counter/end_time
+
+        frame =textWithBackground(frame,f'FPS: {round(fps,1)}',FONTS, 1.0, (30, 50), bgOpacity=0.9, textThickness=2)
+        cv.imshow('frame', frame)
+        key = cv.waitKey(2)
+        if key==ord('q') or key ==ord('Q'):
+            break
+    cv.destroyAllWindows()
+    camera.release()
